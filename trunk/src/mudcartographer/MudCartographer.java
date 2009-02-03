dfcac52e6f0c86850e62aa6fa88509d0b91ca939
cwmaguire <cwmaguire@d33b5a8c-f23d-11dd-99c0-75d572ba1ddd>
2009-02-03 22:15:55 +0000

    Updating all the license preambles in the files

--- trunk/src/mudcartographer/MudCartographer.java
+++ trunk/src/mudcartographer/MudCartographer.java
@@ -1,22 +1,19 @@
 /*
 Copyright 2008 Chris Maguire (cwmaguire@gmail.com)
 
-Lulu's MUD Cartographer is free software: you can redistribute it and/or modify
+MUD Cartographer is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Lulu's MUD Cartographer is distributed in the hope that it will be useful,
+MUD Cartographer is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Lulu's MUD Cartographer.  If not, see <http://www.gnu.org/licenses/>.
-
-August 27, 2008
-*/
-package mudcartographer;
+along with MUD Cartographer.  If not, see <http://www.gnu.org/licenses/>.
+ */package mudcartographer;
 
 import javax.swing.*;
 import java.awt.*;
