b7c70705ea253bbc61f76c0926e3402262d6da7b
cwmaguire <cwmaguire@d33b5a8c-f23d-11dd-99c0-75d572ba1ddd>
2009-02-04 16:19:10 +0000

    IDEA wasn't set to interact with SVN, so I've removed redundant files and added missed files

---
+++ trunk/src/menu/SaveAsMenuItem.java
@@ -0,0 +1,48 @@
+/*
+Copyright 2008 Chris Maguire (cwmaguire@gmail.com)
+
+MUD Cartographer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MUD Cartographer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MUD Cartographer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package menu;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.File;
+
+public class SaveAsMenuItem extends JMenuItem {
+    public SaveAsMenuItem(){
+        setText("Save as ...");
+        setMnemonic(KeyEvent.VK_A);
+
+        setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+        getAccessibleContext().setAccessibleDescription("Save the map in a particular format");
+
+        addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setAcceptAllFileFilterUsed(false);
+                //fileChooser.setFileFilter(new FileNameExtensionFilter());
+                fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("AlexMud World File", ".wld"));
+                fileChooser.showSaveDialog(null);
+                File file = fileChooser.getSelectedFile();
+                System.out.println("File name is " + file.getName());
+            }
+        });
+
+
+    }
+}
